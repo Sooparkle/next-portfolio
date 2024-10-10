37599bd5618540b222382794ac07ed0897787809
Sooparkle <imsoohanmu@gmail.com>
2024-10-10 22:05:52 +0900

    Feat(Header) : Modified Header UI

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,12 +5,14 @@ const menus =[
   {
     id:1,
     url : '/whoami',
-    name : 'whoami'
+    name : 'whoami',
+    svg : `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12.12 12.78C12.05 12.77 11.96 12.77 11.88 12.78C10.12 12.72 8.71997 11.28 8.71997 9.50998C8.71997 7.69998 10.18 6.22998 12 6.22998C13.81 6.22998 15.28 7.69998 15.28 9.50998C15.27 11.28 13.88 12.72 12.12 12.78Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18.74 19.3801C16.96 21.0101 14.6 22.0001 12 22.0001C9.40001 22.0001 7.04001 21.0101 5.26001 19.3801C5.36001 18.4401 5.96001 17.5201 7.03001 16.8001C9.77001 14.9801 14.25 14.9801 16.97 16.8001C18.04 17.5201 18.64 18.4401 18.74 19.3801Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
   },
   {
     id:2,
     url : '/projects',
-    name : 'projects'
+    name : 'projects',
+    svg: `<svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M7.25 6a.75.75 0 00-.75.75v7.5a.75.75 0 001.5 0v-7.5A.75.75 0 007.25 6zM12 6a.75.75 0 00-.75.75v4.5a.75.75 0 001.5 0v-4.5A.75.75 0 0012 6zm4 .75a.75.75 0 011.5 0v9.5a.75.75 0 01-1.5 0v-9.5z"></path><path fill-rule="evenodd" d="M3.75 2A1.75 1.75 0 002 3.75v16.5c0 .966.784 1.75 1.75 1.75h16.5A1.75 1.75 0 0022 20.25V3.75A1.75 1.75 0 0020.25 2H3.75zM3.5 3.75a.25.25 0 01.25-.25h16.5a.25.25 0 01.25.25v16.5a.25.25 0 01-.25.25H3.75a.25.25 0 01-.25-.25V3.75z"></path></g></svg>`
   },
 ]
 
@@ -21,6 +23,7 @@ const Header = () => {
         <h1
           className='hidden'
         >Soohan Portflio</h1>
+        <p>Soohan</p>
       </div>
 
       <section
@@ -34,14 +37,20 @@ const Header = () => {
         </ul>
       </section>
 
-      <nav>
+      <nav
+        className='header-pc-nav'
+      >
         <ul>
         {
           menus.map(menu => (
             <li>
               <Link
                 href={menu.url}
-              >{menu.name}
+              >
+                <span
+                  dangerouslySetInnerHTML={{ __html: menu.svg || '' }}
+                />
+                <span>{menu.name}</span>
               </Link>
             </li>
           ))
