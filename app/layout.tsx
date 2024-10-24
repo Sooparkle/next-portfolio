2c3aeb5c9cfaa4199188bf2ea04f05caddb931ab
soohan <imsoohanmu@gmail.com>
2024-10-24 17:09:26 +0900

    Style(General) : Added Noto serfi and body word-break

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,15 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
+import {Noto_Serif_KR } from 'next/font/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+
+const notoSerifKR = Noto_Serif_KR({
+  weight: ['400', '500', '600','700', '900'], // 사용할 폰트 두께 (일반 및 굵게)
+  subsets: ['latin'],     // 사용할 문자 집합 (라틴 문자가 기본적으로 지원됨)
+  display: 'swap',        // 폰트 로딩 전략 (optional)
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "SooHan Portfolio",
@@ -26,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={notoSerifKR.className}>
         <Header />
         {children}
       </body>
