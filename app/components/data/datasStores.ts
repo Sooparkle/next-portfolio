7cf88d675aaad52ce98b4ddc60f1df766eb0c9c0
Sooparkle <imsoohanmu@gmail.com>
2024-11-12 22:32:51 +0900

    feat : Added data from skills

---
+++ app/components/data/datasStores.ts
@@ -0,0 +1,39 @@
+
+type Skill = string
+
+export const BetterList: Skill[] = [
+  'HTML5',
+  'CSS',
+  'JavaScript',
+  'React js',
+  'HTML5',
+  'CSS',
+  'JavaScript',
+  'React js',
+];
+
+export const GoodList: Skill[] = [
+  'Next js',
+  'TypeScript',
+  'Node js',
+  'git',
+  'Supabase(PostgreSQL)',
+  'Next js',
+  'TypeScript',
+  'Node js',
+  'git',
+  'Supabase(PostgreSQL)',
+];
+
+export const Libraries: Skill[] = [
+  'Tan Stack query',
+  'Zustand',
+  'Chart js',
+  'react-router-dom',
+  'react-calender',
+  'Tan Stack query',
+  'Zustand',
+  'Chart js',
+  'react-router-dom',
+  'react-calender',
+];
